489523bc86d4cdaceeb3268baa50a20b5990e7d5
wujiuye <jiuye.wu@ichestnuts.com>
2020-05-16 01:13:26 +0800

    修正注释； 在单元测试提供使用demo

--- src/main/java/com/wujiuye/miniexcel/excel/annotation/ColumnsDesignator.java
+++ src/main/java/com/wujiuye/miniexcel/excel/annotation/ColumnsDesignator.java
@@ -16,7 +16,7 @@
 package com.wujiuye.miniexcel.excel.annotation;
 
 /**
- * 提供扩展功能，一般使用注解实现，除非有特殊需求，比如：复用一个类型，导出不同标题的excel，或者不同的列
+ * 提供扩展功能
  * 1、自定义忽略哪些字段
  * 2、自定义重命名哪些字段
  *
@@ -42,7 +42,7 @@ public interface ColumnsDesignator {
      *
      * @param fieldName         data的数据类型的字段名
      * @param currentColumnName 当前使用的列名，也就是从注解上面获取的，如果注解没有配置，则等于字段名
-     * @return 不需要重命名则直接返回参数column，需要重命名则返回重命名后的列名（excel文件的标题名称）
+     * @return 不需要重命名则直接返回参数currentColumnName，需要重命名则返回重命名后的列名（excel文件的标题名称）
      */
     String renameColumn(String fieldName, String currentColumnName);
 
