b2daa634f8cd3447b7ec9c5b98ce6ad68024e8e6
wujiuye <jiuye.wu@ichestnuts.com>
2020-05-21 13:46:49 +0800

    添加全局配置文件的支持

--- src/main/java/com/wujiuye/miniexcel/excel/annotation/CellAnnotationParser.java
+++ src/main/java/com/wujiuye/miniexcel/excel/annotation/CellAnnotationParser.java
@@ -16,9 +16,11 @@
 package com.wujiuye.miniexcel.excel.annotation;
 
 
+import com.wujiuye.miniexcel.excel.MiniexcelContent;
 import com.wujiuye.miniexcel.excel.util.StringUtils;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.*;
 
 /**
@@ -59,26 +61,88 @@ public class CellAnnotationParser {
         if (fields.length > 0) {
             for (Field field : fields) {
                 ExcelCellTitle cellTitle = field.getAnnotation(ExcelCellTitle.class);
-                if (cellTitle != null && cellTitle.ignore()) {
+                if (isIgnore(field, cellTitle)) {
                     continue;
                 }
                 ExcelMetaData metaData = new ExcelMetaData();
                 metaData.setField(field);
+                excelMetaData.add(metaData);
                 if (cellTitle == null || StringUtils.isEmpty(cellTitle.alias())) {
                     metaData.setCellName(field.getName());
                 } else {
                     metaData.setCellName(cellTitle.alias());
                 }
-                if (field.getType() == Date.class) {
-                    if (cellTitle != null && !StringUtils.isEmpty(cellTitle.datePattern())) {
-                        metaData.setDatePattern(cellTitle.datePattern());
-                        metaData.setTimezone(cellTitle.timeZone());
-                    }
-                }
-                excelMetaData.add(metaData);
+                applyDateCfg(field, cellTitle, metaData);
             }
         }
         return excelMetaData;
+    }
+
+    /**
+     * 忽略字段
+     * 条件：
+     * 1、注解不为空且注解的ignore为true
+     * 2、全局配置了忽略不存在注解字段，且注解为空
+     *
+     * @param field     字段
+     * @param cellTitle 字段上的注解
+     * @return
+     */
+    private static boolean isIgnore(Field field, ExcelCellTitle cellTitle) {
+        if (cellTitle != null && cellTitle.ignore()) {
+            return true;
+        }
+        GlobalCfgProperties properties = MiniexcelContent.getGlobalCfgProperties();
+        if (properties == null) {
+            return false;
+        }
+        if (Boolean.TRUE.equals(properties.getIgnore_not_exist_annotation_field())
+                && cellTitle == null) {
+            return true;
+        }
+        return Boolean.TRUE.equals(properties.getIgnore_transient_field())
+                && (field.getModifiers() & Modifier.TRANSIENT) == Modifier.TRANSIENT;
+    }
+
+    /**
+     * 如果字段的类型为Date，则初始化日期格式化和时区配置
+     *
+     * @param field     字段
+     * @param cellTitle 注解
+     * @param metaData  元数据
+     */
+    private static void applyDateCfg(Field field, ExcelCellTitle cellTitle, ExcelMetaData metaData) {
+        if (field.getType() != Date.class) {
+            return;
+        }
+        // 注解优先
+        if (cellTitle != null) {
+            if (!StringUtils.isEmpty(cellTitle.datePattern())) {
+                metaData.setDatePattern(cellTitle.datePattern());
+            }
+            if (cellTitle.timeZone() >= -12 && cellTitle.timeZone() <= 13) {
+                metaData.setTimezone(cellTitle.timeZone());
+            }
+        }
+        // 全局配置
+        GlobalCfgProperties properties = MiniexcelContent.getGlobalCfgProperties();
+        if (properties != null && properties.getDate_global_cfg() != null) {
+            if (StringUtils.isEmpty(metaData.getDatePattern())
+                    && properties.getDate_global_cfg().getDate_pattern() != null) {
+                metaData.setDatePattern(properties.getDate_global_cfg().getDate_pattern());
+            }
+            if (metaData.getTimezone() == null
+                    && properties.getDate_global_cfg().getTime_zone() != null) {
+                metaData.setTimezone(properties.getDate_global_cfg().getTime_zone());
+            }
+        }
+        // 默认
+        if (StringUtils.isEmpty(metaData.getDatePattern())) {
+            metaData.setDatePattern("yyyy-MM-dd HH:mm:ss");
+        }
+        if (metaData.getTimezone() == null) {
+            metaData.setTimezone(8);
+        }
     }
 
     /**
